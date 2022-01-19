01b4142bea36024085eb1ef7be06a21aea767200
Brooklyn92 <Tubanar173russia>
2022-01-19 17:51:40 +0400

    add setProperty

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
 public class MainTest {
+    @Test
+    void mainTest(){
+        System.setProperty("webdriver.chrome.driver","C:/webdrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://yandex.ru/");
+    }
 }
