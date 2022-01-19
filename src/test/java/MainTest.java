af9967f7ff15be3b9e43f438d1b0f08be6ec6234
Brooklyn92 <Tubanar173russia>
2022-01-19 17:53:11 +0400

    delete import

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chromium.ChromiumDriver;
 
 public class MainTest {
     @Test
