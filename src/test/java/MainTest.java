cceb478280bc1b30cd2a2d29c8efaa6dda1ea1a5
Aleksandr <yurchenko221292@mail.ru>
2022-01-20 23:49:03 +0400

    part1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,14 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainTest {
     @Test
-    void mainTest(){
-        System.setProperty("webdriver.chrome.driver","C:/webdrivers/chromedriver.exe");
+    void mainTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/webdrivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://yandex.ru/");
-
+        try {
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // на каждый поиск элемента будет искаться не более 10 секунд
+            driver.manage().window().maximize();
+            driver.get("https://yandex.ru/");
+            WebElement inputSearch = driver.findElement(By.name("text"));
+            inputSearch.sendKeys("Arcane", Keys.ENTER); // search word Arcane and press enter
+        } finally {
+            driver.close();
+        }
     }
 }
